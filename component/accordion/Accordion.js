f9f70562e96a2d4eddf995bbe20d76090120d577
MohammadHossein <vafaee.mh@gmail.com>
2023-02-07 07:47:26 +0330

    finish accrdion

--- component/accordion/Accordion.js
+++ component/accordion/Accordion.js
@@ -1,12 +1,34 @@
 import Image from "next/image";
+import { useState } from "react";
 import classes from "../../styles/accordion.module.css";
 import { qustions } from "../img/image";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 const Accordion = ({ data }) => {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className={classes.wrapper}>
-      <div className={classes.accordion}></div>
+      <div>
+        {data.map((item) => (
+          <div className={classes.accordion}>
+            <div className={classes.boxTitle}
+              onClick={() => {
+                item.status = !item.status;
+                setIsActive(!isActive);
+              }}
+            >
+              <span className={classes.boxIcon}>{isActive & item.status ? <SlArrowUp className={classes.icon} /> : <SlArrowDown />}</span>
+              <h2>{item.title}</h2>
+            </div>
+            {isActive & item.status ? (
+              <div className={classes.content}>{item.content}</div>
+            ) : (
+              ""
+            )}
+          </div>
+        ))}
+      </div>
       <div className={classes.boxImage}>
-        <Image className={classes.image} src={qustions}/>
+        <Image className={classes.image} src={qustions} />
       </div>
     </div>
   );
