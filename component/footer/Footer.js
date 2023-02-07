bd07ed4cf077cf0308fde8140c82d1f56fe8ac87
MohammadHossein <vafaee.mh@gmail.com>
2023-02-07 08:09:20 +0330

    start create footer for project

---
+++ component/footer/Footer.js
@@ -0,0 +1,9 @@
+import classes from "../../styles/footer.module.css"
+const Footer = () => {
+  return (
+    <div className={classes.footer}>
+      <div></div>
+    </div>
+  );
+};
+export default Footer;
